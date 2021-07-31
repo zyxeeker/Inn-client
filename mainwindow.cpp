970eaff18c3cc74d165fd86f835c19ebc07dd1d5
zyxeeker <zyxeeker@gmail.com>
2021-07-31 18:28:34 +0800

    ✔[add]主界面逻辑

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,10 +6,72 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    setWindowFlag(Qt::FramelessWindowHint);
+    setAttribute(Qt::WA_TranslucentBackground, true);
+    ImportContentBackground();
 }
 
-MainWindow::~MainWindow()
-{
+MainWindow::~MainWindow() {
     delete ui;
 }
 
+void MainWindow::ImportContentBackground() {
+    QSvgRenderer r(QString(":/common/resource/bk_test.svg"));
+    m_px = new QPixmap(ui->content->size());
+    QPainter Painter;
+    m_px->fill(Qt::transparent);
+    Painter.begin(m_px);
+    r.render(&Painter);
+    Painter.end();
+}
+
+void MainWindow::SetContentBackground() {
+    ui->content->setAutoFillBackground(true);
+    QPalette palette = ui->content->palette();
+    palette.setBrush(QPalette::Window,
+                     QBrush(m_px->scaled(ui->content->size(), Qt::IgnoreAspectRatio, Qt::SmoothTransformation)));
+    ui->content->setPalette(palette);
+}
+
+void MainWindow::resizeEvent(QResizeEvent *e) {
+#if 0
+    QSvgRenderer r(QString(":/common/resource/bk_test.svg"));
+    QPixmap px(ui->content->size());
+    QPainter Painter;
+
+    px.fill(Qt::transparent);
+    Painter.begin(&px);
+    r.render(&Painter);
+    Painter.end();
+
+    ui->content->setAutoFillBackground(true);
+    QPalette palette = ui->content->palette();
+    palette.setBrush(QPalette::Window,QBrush(m_px->scaled(ui->content->size(),Qt::IgnoreAspectRatio,Qt::SmoothTransformation)));
+    ui->content->setPalette(palette);
+#endif
+    SetContentBackground();
+}
+
+void MainWindow::mousePressEvent(QMouseEvent *e) {
+    m_titleXMax = ui->title->width();
+    if (e->button() == Qt::LeftButton) {
+        m_mousePosition = e->pos();
+        if (m_mousePosition.x() <= m_titleXMin) return;
+        if (m_mousePosition.x() >= m_titleXMax) return;
+        if (m_mousePosition.y() <= m_titleYMin) return;
+        if (m_mousePosition.y() >= m_titleYMax) return;
+        m_mousePress = true;
+    }
+
+}
+
+void MainWindow::mouseMoveEvent(QMouseEvent *e) {
+    if (m_mousePress) {
+        QPoint position = e->globalPos();
+        move(position - m_mousePosition);
+    }
+}
+
+void MainWindow::mouseReleaseEvent(QMouseEvent *e) {
+    m_mousePress = false;
+}
--- mainwindow.h
+++ mainwindow.h
@@ -2,20 +2,47 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QPixmap>
+#include <QPainter>
+#include <QMouseEvent>
+#include <QtSvg/QSvgRenderer>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
-class MainWindow : public QMainWindow
-{
-    Q_OBJECT
+class MainWindow : public QMainWindow {
+Q_OBJECT
 
 public:
     MainWindow(QWidget *parent = nullptr);
+
     ~MainWindow();
+
+protected:
+    void resizeEvent(QResizeEvent *e);
+
+    void mouseMoveEvent(QMouseEvent *e);
+
+    void mousePressEvent(QMouseEvent *e);
+
+    void mouseReleaseEvent(QMouseEvent *e);
 
 private:
     Ui::MainWindow *ui;
+    QPixmap *m_px;
+
+    const int m_titleXMin = 0;
+    const int m_titleYMin = 0;
+    int m_titleXMax = 800;
+    const int m_titleYMax = 30;
+
+    bool m_mousePress;
+    QPoint m_mousePosition;
+
+private:
+    void SetContentBackground();
+
+    void ImportContentBackground();
 };
 #endif // MAINWINDOW_H
