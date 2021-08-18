ba8e44ae2fb6ed19c6da01d2ba16d786a189dba8
zyxeeker <zyxeeker@gmail.com>
2021-08-18 11:08:52 +0800

    🛠[update]聊天内容框基本构造

--- component/chat_content.cpp
+++ component/chat_content.cpp
@@ -3,3 +3,54 @@
 //
 
 #include "chat_content.h"
+
+AbstractChatContent::AbstractChatContent() {
+    m_contentTime = new QLabel;
+    m_content = new QTextBrowser;
+    m_contentLayout = new QHBoxLayout;
+}
+
+IconChatContent::IconChatContent() {
+    Init();
+}
+
+void IconChatContent::Init() {
+    m_userIcon = new QLabel;
+    m_userName = new QLabel;
+    m_contentTitleLayout = new QHBoxLayout;
+    m_contentDetailLayout = new QVBoxLayout;
+    m_iconLayout = new QVBoxLayout;
+
+    m_userIcon->setMinimumSize(50, 50);
+    m_userIcon->setMaximumSize(50, 50);
+    m_userIcon->setStyleSheet("background:black;");
+    m_userName->setText("AAABBBASBAB");
+    m_contentTime->setText("2021/8/18");
+
+    m_iconLayout->addWidget(m_userIcon);
+    m_iconLayout->setAlignment(Qt::AlignTop);
+
+    m_contentTitleLayout->addWidget(m_userName);
+    m_contentTitleLayout->addWidget(m_contentTime);
+    m_contentTitleLayout->setAlignment(Qt::AlignLeft);
+    m_contentTitleLayout->setSpacing(8);
+    m_contentDetailLayout->addItem(m_contentTitleLayout);
+    m_contentDetailLayout->addWidget(m_content);
+    m_contentLayout->addLayout(m_iconLayout);
+    m_contentLayout->addLayout(m_contentDetailLayout);
+    m_content->setContentsMargins(0, 0, 0, 0);
+
+    this->setLayout(m_contentLayout);
+
+}
+
+ChatContent::ChatContent() {
+    Init();
+}
+
+void ChatContent::Init() {
+    m_contentTime->setText("2021/8/18");
+    m_contentLayout->addWidget(m_contentTime);
+    m_contentLayout->addWidget(m_content);
+    this->setLayout(m_contentLayout);
+}
--- component/chat_content.h
+++ component/chat_content.h
@@ -6,11 +6,46 @@
 #define INNCLIENT_CHAT_CONTENT_H
 
 #include <QWidget>
+#include <QLabel>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QTextBrowser>
 
 class AbstractChatContent : public QWidget {
 Q_OBJECT
+public:
+    AbstractChatContent();
+
+protected:
+    QLabel *m_contentTime;
+    QTextBrowser *m_content;
+    QHBoxLayout *m_contentLayout;
 
 };
 
+class IconChatContent : public AbstractChatContent {
+public:
+    IconChatContent();
+
+private:
+    void Init();
+
+private:
+    QLabel *m_userIcon;
+    QLabel *m_userName;
+
+    QVBoxLayout *m_iconLayout;
+    QHBoxLayout *m_contentTitleLayout;
+    QVBoxLayout *m_contentDetailLayout;
+};
+
+class ChatContent : public AbstractChatContent {
+public:
+    ChatContent();
+
+private:
+    void Init();
+
+};
 
 #endif //INNCLIENT_CHAT_CONTENT_H
