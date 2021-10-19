7e525ca000a179b2d3e1ed6c9583bdb767802dfc
zyxeeker <zyxeeker@gmail.com>
2021-10-19 16:30:04 +0800

    ✔[add]加入DevTools

--- cef/cef_widget.cpp
+++ cef/cef_widget.cpp
@@ -16,18 +16,17 @@
 
 
 CefWidget::CefWidget(QWidget *parent) : QWidget(parent) {
-    m_window = new QWindow(windowHandle());
-    m_layout = new QVBoxLayout(this);
-    m_container = QWidget::createWindowContainer(m_window);
     std::string url = "www.google.com";
-    m_handler = new CefHandler(this);
     InitUi();
     CefRect pRect{this->x(), this->y(), this->width(), this->height()};
     CefBrowserSettings pSettings;
-    CreateBrowser(pRect, pSettings, nullptr, nullptr);
 }
 
 void CefWidget::InitUi() {
+    m_window = new QWindow(windowHandle());
+    m_layout = new QVBoxLayout(this);
+    m_container = QWidget::createWindowContainer(m_window);
+    m_handler = new CefHandler(this);
     m_layout->setContentsMargins(0, 0, 0, 0);
     m_layout->setSpacing(0);
     m_layout->addWidget(m_container);
@@ -35,20 +34,27 @@ void CefWidget::InitUi() {
 
 bool CefWidget::CreateBrowser(const CefRect &rect, const CefBrowserSettings &settings,
                               CefRefPtr<CefDictionaryValue> extra_info, CefRefPtr<CefRequestContext> request_context) {
-    CefWindowInfo pInfo;
     RECT pRect = {rect.x, rect.y, rect.x + rect.width, rect.y + rect.height};
-    pInfo.SetAsChild((CefWindowHandle) m_window->winId(), pRect);
-    return CefBrowserHost::CreateBrowser(pInfo, m_handler, "file:///D:/program/Inn-client/index.html", settings,
+    m_info.SetAsChild((CefWindowHandle) m_window->winId(), pRect);
+    return CefBrowserHost::CreateBrowser(m_info, m_handler, "chrome://version", settings,
                                          extra_info, request_context);
 }
 
 void CefWidget::ResizeBrowser(QSize size) {
-    ::MoveWindow(m_winHandle, 0, 0, size.width(), size.height(), false);
-    ::SetWindowPos(m_winHandle, HWND_BOTTOM, 0, 0, size.width(), size.height(), SWP_NOACTIVATE);
+    if (m_browser) {
+        ::MoveWindow(m_winHandle, 0, 0, size.width(), size.height(), false);
+        ::SetWindowPos(m_winHandle, HWND_BOTTOM, 0, 0, size.width(), size.height(), SWP_NOACTIVATE);
+    }
 }
 
 void CefWidget::resizeEvent(QResizeEvent *event) {
-    ResizeBrowser(event->size());
+    if (!m_createMark) {
+        CefRect rect{x(), y(), x() + event->size().width(), y() + event->size().height()};
+        CefBrowserSettings pBrowserSettings;
+        CreateBrowser(rect, pBrowserSettings, nullptr, nullptr);
+        m_createMark = true;
+    } else
+        ResizeBrowser(event->size());
 }
 
 void CefWidget::closeEvent(QCloseEvent *event) {
@@ -82,3 +88,45 @@ bool CefWidget::BrowserKeyEvent(CefRefPtr<CefBrowser> browser, const CefKeyEvent
             emit OpenDevTools();
     return false;
 }
+
+CefDevTools::CefDevTools(CefRefPtr<CefBrowser> browser) : m_targetBrowser(browser) {
+    m_window = new QWindow(windowHandle());
+    m_layout = new QVBoxLayout(this);
+    m_devHandler = new CefHandler(this);
+    InitUi();
+    this->setAttribute(Qt::WA_DeleteOnClose);
+}
+
+void CefDevTools::BrowserCreated(CefRefPtr<CefBrowser> browser) {
+    m_devBrowser = browser;
+    m_winHandle = browser->GetHost()->GetWindowHandle();
+}
+
+void CefDevTools::InitUi() {
+    m_container = QWidget::createWindowContainer(m_window);
+    m_layout->setContentsMargins(0, 0, 0, 0);
+    m_layout->setSpacing(0);
+    m_layout->addWidget(m_container);
+}
+
+void CefDevTools::Resize(QSize size) {
+    if (m_devBrowser) {
+        ::MoveWindow(m_winHandle, 0, 0, size.width(), size.height(), false);
+        ::SetWindowPos(m_winHandle, HWND_BOTTOM, 0, 0, size.width(), size.height(), SWP_NOACTIVATE);
+    }
+}
+
+void CefDevTools::resizeEvent(QResizeEvent *event) {
+    if (!m_createdMark) {
+        CefBrowserSettings pBrowserSettings;
+        CefWindowInfo pWindowInfo;
+
+        RECT pRect = {0, 0, event->size().width(), event->size().height()};
+        pWindowInfo.SetAsChild((CefWindowHandle) m_window->winId(), pRect);
+
+        m_targetBrowser->GetHost()->ShowDevTools(pWindowInfo, m_devHandler, pBrowserSettings, CefPoint());
+        m_createdMark = true;
+    }
+    Resize(event->size());
+}
+
